3f6cb9502aaa896bf2cdb86a94a7179f80ea41e9
MatthewR117 <matthewrey115@gmail.com>
2026-04-08 19:29:35 -0500

    Add files via upload

---
+++ Main/main.py
@@ -0,0 +1,8 @@
+# Main file for Digital Audio Post Processor
+# Launches the GUI + DSP 
+# Created by Matthew Reyna, Cadden Craddock, and Justin Furnas
+from gui import main
+
+if __name__ == "__main__":
+    main()
+# end :)
